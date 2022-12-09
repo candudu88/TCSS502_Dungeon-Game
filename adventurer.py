a446b487694347992faecd74c2dea09e9b41bf59
jesse <itsjessef@gmail.com>
2022-12-09 01:25:48 -0800

    implement adventurer class

--- adventurer.py
+++ adventurer.py
@@ -1,2 +1,21 @@
 class Adventurer:
-    pass
+    def __init__(self, name="Player"):
+        self.__name = name
+        self.__hit_points = 100
+        self.__healing_potion_count = 0
+        self.__vision_potion_count = 0
+        self.__pillars_found = []
+
+    def increase_hit_point(self, amount):
+        self.__hit_points = max(0, self.__hit_points - amount)
+
+    def decrease_hit_point(self, amount):
+        self.__hit_points = min(100, self.__hit_points + amount)
+
+    def __str__(self):
+        return f"Name: {self.name}\n" \
+               f"Hit points: {self.__hit_points}\n" \
+               f"Healing potions: {self.__healing_potion_count}" \
+               f"Vision potions: {self.__vision_potion_count}\n" \
+               f"Pillars found: {self.__pillars_found}"
+

5b57f90ae93d1cf51cf05fcd598dccf8f4190b3f
jesse <itsjessef@gmail.com>
2022-12-11 21:02:15 -0800

    add additonal tests; remove blank test

--- tst/dungeon-tests.py
+++ tst/dungeon-tests.py
@@ -10,6 +10,33 @@ class DungeonTests(unittest.TestCase):
     This class tests functionality Dungeon class.
     """
 
+    def test_dungeon_init(self):
+        """Test instance of Dungeon has rows and columns set"""
+        dungeon = Dungeon(4, 4)
+        attributes = {"rows": dungeon.rows,
+                      "cols": dungeon.cols}
+        expected_attributes = {"rows": 4,
+                               "cols": 4}
+        self.assertDictEqual(expected_attributes, attributes, "expected rows and cols attributes were not set")
+
+    def test_dungeon_has_rooms_of_specified_rows_and_cols(self):
+        """Test dungeon has 2D array of specified rows and columns"""
+        dungeon = Dungeon(2, 8)
+        grid = {"rows": len(dungeon.rooms), "columns": len(dungeon.rooms[0])}
+        expected_grid = {"rows": 2, "columns": 8}
+        self.assertDictEqual(expected_grid, grid, "expected 2 rows and 8 columns")
+
+    def test_created_dungeon_has_rooms(self):
+        """Test dungeon has rooms in every slot"""
+        dungeon = Dungeon(4, 4)
+        has_rooms = True
+        for i in range(4):
+            for j in range(4):
+                if str(Room(0, 0)) != str(dungeon.rooms[i][j]):
+                    has_rooms = False
+                    break
+        self.assertEqual(True, has_rooms, "Not every space in Dungeon has a room")
+
     def test_get_room(self):
         """Test get_room method of Dungeon"""
         dungeon = Dungeon(5, 5)
@@ -20,25 +47,14 @@ class DungeonTests(unittest.TestCase):
                 expected_rooms[i].append(Room(i, j))
         self.assertEqual(str(expected_rooms), str(dungeon.get_room()), "expected [] with 25 empty rooms")
 
-    def test_print(self):
-        """Test print method of Dungeon"""
-        dungeon = Dungeon(5, 5)
-        # dungeon.print()
-        # write test for print statements, "expected print does not match format"
-
-    def test_draw(self):
-        """Test draw method of Dungeon"""
-        dungeon = Dungeon(5, 5)
-        # dungeon.draw()
-        # write test for print statements
-
     def test_get_neighbors(self):
         """Test get neighbors of Room at row:0 col:0"""
         dungeon = Dungeon(4, 4)
         room = dungeon.rooms[0][0]
         output_neighbors = dungeon.get_neighbors(room, [])
         expected_neighbors = [Room(0, 1), Room(1, 0)]
-        self.assertEqual(str(expected_neighbors), str(output_neighbors), "expected two empty rooms as neighbors, 1 east and 1 south")
+        self.assertEqual(str(expected_neighbors), str(output_neighbors), "expected two empty rooms as neighbors, "
+                                                                         "1 east and 1 south")
 
     def test_create_doors(self):
         """Test creation of doors in a room of Dungeon"""
